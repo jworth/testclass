30509b20a71f6aebc22c03cbd6b8cb4a96645178
tombartindale <tom@bartindale.com>
2017-05-03 13:23:16 +0100

    pull elevator dist

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /tmp
+git clone git@github.com:connectedacademy/elevator.git
+cp /tmp/docs /connectedacademy
+
+# upgrade changes
+git config --global user.name "CircleCI"
+git config --global user.email "info@connectedacademy.io"
+git checkout -b staging
+git add .
+git commit -am"[skip CI] AUTOBUILD"
+git checkout -b gh-pages
+git rebase -Xtheirs staging
+# git push -u -f origin gh-pages
