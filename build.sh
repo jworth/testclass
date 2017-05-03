7cec7c3c91f39c643ebadd3382d9475e1b229b88
tombartindale <tom@bartindale.com>
2017-05-03 13:45:21 +0100

    test

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 cd /tmp
 git clone git@github.com:connectedacademy/elevator.git
-cp /tmp/elevator/docs ~/connectedacademy
+ls /tmp/elevator
+cp -r /tmp/elevator/docs ~/connectedacademy
 
 # upgrade changes
 git config --global user.name "CircleCI"
